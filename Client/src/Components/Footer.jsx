6bec5b8aa8a831cdaf356159fdd40b0850bd6877
degbessou <dedokema@gmail.com>
2025-10-03 00:07:06 -0400

    footer fix

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -20,7 +20,7 @@ export default () => {
     ]
     return (
         <footer className="pt-10">
-            <div className="max-w-screen-xl mx-auto px-4 md:px-8 text-yellow-700">
+            <div className="max-w-screen-lg mx-auto px-4 md:px-8 text-yellow-700">
                 <div className="mt-10 py-10 border-t items-center justify-between sm:flex">
                     <p>© 2025 Soccer Bénin. All rights reserved.</p>
                     <ul className="flex flex-wrap items-center gap-4 mt-6 sm:text-sm sm:mt-0">
