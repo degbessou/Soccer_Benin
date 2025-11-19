0f297361ea372fc991228486a9627b4346519f5b
degbessou <dedokema@gmail.com>
2025-11-18 21:47:43 -0500

    added pagination to news page

--- Client/src/Components/Cards.jsx
+++ Client/src/Components/Cards.jsx
@@ -1,22 +1,27 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { supabase } from "../Functions/SupabaseClient"
 import { getSupabaseImageUrl } from "../assets/Helpers"
 import { getPreposition } from "../assets/Helpers"
+import Pagination from "../assets/Pagination"
 
 export default function Cards() {
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
     const [groupedActus, setGroupedActus] = useState({})
+    const [currentPage, setCurrentPage] = useState(1)
+    const topRef = useRef(null)
+
 
     useEffect(() => {
         async function fetchnews() {
             setLoading(true)
             setError(null)
+
             const { data, error } = await supabase
                 .from("news")
                 .select("*")
                 .order("years", { ascending: false })
-                .order("month_number", { ascending: false })  // ← Utilise month_number
+                .order("month_number", { ascending: false })
                 .order("date", { ascending: false })
                 .order("id", { ascending: false })
 
@@ -33,63 +38,81 @@ export default function Cards() {
             }
             setLoading(false)
         }
+
         fetchnews()
     }, [])
+
+    useEffect(() => {
+        if (topRef.current) {
+            topRef.current.scrollIntoView({ behavior: "smooth" })
+        }
+    }, [currentPage])
+
 
     if (loading) return <p className="text-center text-gray-500 animate-pulse">Chargement des actualités...</p>
     if (error) return <p className="text-center text-red-600">❌ Erreur : {error}</p>
 
+    // Pagination logic
+    const monthKeys = Object.keys(groupedActus)
+    const totalPages = monthKeys.length
+    const currentMonthKey = monthKeys[currentPage - 1]
+    const currentNews = groupedActus[currentMonthKey] || []
+
     return (
         <section className="py-4">
             <div className="max-w-screen-lg mx-auto px-4 md:px-8">
-                {Object.entries(groupedActus).map(([title, news]) => {
-                    const month = title.split(' ')[0]
-                    const year = title.split(' ')[1]
-                    return (
-                        <div key={title} className="mb-10">
-                            <h1 className="text-gray-800 text-2xl font-extrabold sm:text-3xl mb-4">
-                                L'actualité {getPreposition(month)}{month} {year}
-                            </h1>
-                            <ul>
-                                {news.map((item, idx) => (
-                                    <li key={idx} className="py-2 hover:bg-green-50 rounded-xl transition-colors">
-                                        <div className="px-4 flex item-center">
-                                            <span className="text-sm w-32 font-semibold">{item.date}</span>
-                                            <h3 className="text-red-700 text-sm font-semibold ">
-                                                {item.tag}
-                                            </h3>
-                                            <h3 className="text-red-700 text-sm font-semibold ">
-                                                {item.tag_un}
-                                            </h3>
-                                            <h3 className="text-red-700 text-sm font-semibold">
-                                                {item.tag_deux}
-                                            </h3>
-                                            {/*<div className="bg-white w-4 h-4 pt-1 flex items-center justify-center">
-                                                <img src={item.icon} alt="icon" className="w-4 h-4" />
-                                            </div> */}
+                <div ref={topRef}></div>
+                {/* TITRE DU MOIS */}
+                <div className="mb-10">
+                    <h1 className="text-gray-800 text-2xl font-extrabold sm:text-3xl mb-4">
+                        L'actualité {getPreposition(currentMonthKey.split(" ")[0])}
+                        {currentMonthKey}
+                    </h1>
+
+                    {/* LISTE DES NEWS */}
+                    <ul>
+                        {currentNews.map((item, idx) => (
+                            <li
+                                key={idx}
+                                className="py-2 hover:bg-green-50 rounded-xl transition-colors"
+                            >
+                                <div className="px-4 flex items-center gap-x-3">
+                                    <span className="text-sm w-28 font-semibold">{item.date}</span>
+
+                                    <h3 className="text-red-700 text-sm font-semibold">{item.tag}</h3>
+                                    <h3 className="text-red-700 text-sm font-semibold">{item.tag_un}</h3>
+                                    <h3 className="text-red-700 text-sm font-semibold">{item.tag_deux}</h3>
+                                </div>
+
+                                {/* IMAGE */}
+                                {item.has_image ? (
+                                    <div className="grid grid-cols-[auto_1fr] gap-4 pt-2">
+                                        <div className="w-[185px] h-[105px] flex items-center justify-center bg-gray-100 rounded-md overflow-hidden">
+                                            <img
+                                                src={getSupabaseImageUrl(item.image)}
+                                                alt="Image"
+                                                className="w-full h-full object-cover"
+                                            />
                                         </div>
-                                        {item.has_image ? (
-                                            <div className="grid grid-cols-[auto_1fr] gap-4 pt-2">
-                                                <div className="w-[185px] h-[105px] flex items-center justify-center bg-gray-100 rounded-md overflow-hidden">
-                                                    <img
-                                                        src={getSupabaseImageUrl(item.image)}
-                                                        alt="Image"
-                                                        className="w-full h-full object-cover"
-                                                    />
-                                                </div>
-                                                <p className="text-sm">{item.infos}</p>
-                                            </div>
-                                        ) : (
-                                            <div className="pt-2">
-                                                <p className="text-sm">{item.infos}</p>
-                                            </div>
-                                        )}
-                                    </li>
-                                ))}
-                            </ul>
-                        </div>
-                    )
-                })}
+                                        <p className="text-sm">{item.infos}</p>
+                                    </div>
+                                ) : (
+                                    <div className="pt-2">
+                                        <p className="text-sm">{item.infos}</p>
+                                    </div>
+                                )}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+
+                {/* PAGINATION */}
+                <Pagination
+                    totalPages={totalPages}
+                    currentPage={currentPage}
+                    onPageChange={setCurrentPage}
+                />
+
             </div>
         </section>
     )
---
+++ Client/src/assets/Pagination.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+export default function Pagination({ totalPages, currentPage, onPageChange }) {
+    const pages = Array.from({ length: totalPages }, (_, i) => (i + 1).toString());
+
+    return (
+        <div className="max-w-screen-xl mx-auto mt-12 px-4 text-gray-600 md:px-8">
+            <div className="hidden items-center justify-between sm:flex" aria-label="Pagination">
+                <button
+                    onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+                    className="hover:text-yellow-700 flex items-center gap-x-2"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                        <path
+                            fillRule="evenodd"
+                            d="M18 10a.75.75 0 01-.75.75H4.66l2.1 1.95a.75.75 0 11-1.02 1.1l-3.5-3.25a.75.75 0 010-1.1l3.5-3.25a.75.75 0 111.02 1.1l-2.1 1.95h12.59A.75.75 0 0118 10z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                    Précédent
+                </button>
+
+                <ul className="flex items-center gap-1">
+                    {pages.map((item) => (
+                        <li key={item} className="text-sm">
+                            <button
+                                onClick={() => onPageChange(parseInt(item))}
+                                aria-current={currentPage == item ? "page" : undefined}
+                                className={`px-3 py-2 rounded-lg duration-150 hover:text-yellow-700 hover:bg-yellow-50 ${currentPage == item ? "bg-yellow-50 text-yellow-700 font-medium" : ""
+                                    }`}
+                            >
+                                {item}
+                            </button>
+                        </li>
+                    ))}
+                </ul>
+
+                <button
+                    onClick={() => currentPage < totalPages && onPageChange(currentPage + 1)}
+                    className="hover:text-yellow-700 flex items-center gap-x-2"
+                >
+                    Suivant
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
+                        <path
+                            fillRule="evenodd"
+                            d="M2 10a.75.75 0 01.75-.75h12.59l-2.1-1.95a.75.75 0 111.02-1.1l3.5 3.25a.75.75 0 010 1.1l-3.5 3.25a.75.75 0 11-1.02-1.1l2.1-1.95H2.75A.75.75 0 012 10z"
+                            clipRule="evenodd"
+                        />
+                    </svg>
+                </button>
+            </div>
+            {/* Mobile */}
+            <div className="flex items-center justify-between text-sm text-gray-600 font-medium sm:hidden">
+                <button
+                    onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+                    className="px-4 py-2 border rounded-lg duration-150 hover:bg-yellow-50"
+                >
+                    Précédent
+                </button>
+
+                <div className="font-medium">Page {currentPage} sur {totalPages}</div>
+
+                <button
+                    onClick={() => currentPage < totalPages && onPageChange(currentPage + 1)}
+                    className="px-4 py-2 border rounded-lg duration-150 hover:bg-yellow-50"
+                >
+                    Suivant
+                </button>
+            </div>
+        </div>
+    );
+}
