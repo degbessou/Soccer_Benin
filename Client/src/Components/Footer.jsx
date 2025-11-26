39b0142293cd22fc1ffa3ea069e9c86d5cf4820d
degbessou <dedokema@gmail.com>
2025-11-25 22:38:57 -0500

    Minor adjustment

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -26,7 +26,7 @@ export default () => {
                     <ul className="flex flex-wrap items-center gap-4 mt-6 sm:text-sm sm:mt-0">
                         {
                             footerNavs.map((item, idx) => (
-                                <li key={idx} className="hover:text-yellow-400 duration-150">
+                                <li key={idx} className="hover:font-semibold duration-150">
                                     <a href={item.href}>
                                         {item.name}
                                     </a>
