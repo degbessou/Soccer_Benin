8ac011127af4925d4b33deab765798114aea8391
degbessou <dedokema@gmail.com>
2025-11-29 01:54:21 -0500

    Minors adjustements

--- Client/src/Components/Footer.jsx
+++ Client/src/Components/Footer.jsx
@@ -22,7 +22,7 @@ export default () => {
         <footer className="pt-4">
             <div className="max-w-screen-lg mx-auto px-4 md:px-8 text-yellow-700">
                 <div className="mt-10 py-10 border-t items-center justify-between sm:flex">
-                    <p>© 2025 Soccer Bénin. All rights reserved.</p>
+                    <p>© {new Date().getFullYear()} BencoFoot. All rights reserved.</p>
                     <ul className="flex flex-wrap items-center gap-4 mt-6 sm:text-sm sm:mt-0">
                         {
                             footerNavs.map((item, idx) => (
